d50fef28b31b8ddaa00331c82bebe66b7a7bd804
ahmedbashir10 <ahmedhaadibashiir@gmail.com>
2024-02-22 21:45:12 +0100

    done implementing transactions

--- back_end/src/services/authService.ts
+++ back_end/src/services/authService.ts
@@ -1,6 +1,7 @@
 /* eslint-disable @typescript-eslint/no-extraneous-class */
-import bcrypt from 'bcrypt'
-import User from '../model/user'
+import bcrypt from "bcrypt"
+import User from "../model/user"
+import db from "../integration/dbConfig"
 /**
  * Defines the structure for login credentials.
  * @typedef {Object} LoginCredentials
@@ -8,18 +9,18 @@ import User from '../model/user'
  * @property {string} password - User's password.
  */
 interface LoginCredentials {
-  username: string;
-  password: string;
+  username: string
+  password: string
 }
 interface registerCredentials {
-  person_id: number;
-  name: string;
-  surname: string;
-  pnr: string;
-  email: string;
-  username: string;
-  password: string;
-  role_id: number;
+  person_id: number
+  name: string
+  surname: string
+  pnr: string
+  email: string
+  username: string
+  password: string
+  role_id: number
 }
 /**
  * Service class for authentication-related operations.
@@ -34,68 +35,88 @@ class AuthService {
    * @returns {Promise<Person | null>} - A promise that resolves to the user object if login is successful, or null if the login fails.
    * @throws {Error} - Throws an error if the login process fails due to an unexpected error.
    */
-  public static async login ({ username, password }: LoginCredentials): Promise<User | null> {
+  public static async login({
+    username,
+    password,
+  }: LoginCredentials): Promise<User | null> {
     try {
-      let user: User | null = await User.findOne({ where: { username } });
-
-      if (user == null) {
-        user = await User.findOne({ where: { email: username } });
+      return await db.transaction(async () => {
+        let user: User | null =
+          (await User.findOne({ where: { username } })) ||
+          (await User.findOne({ where: { email: username } }))
 
         if (user == null) {
-          throw new Error("Username or email does not exist.");
+          throw new Error("Username or email does not exist.")
         }
-      }
 
-      // Recruiter
-      // if (user.role_id == "1") {
-      //   const isMatch = await User.findOne({ where: { password: user.password } });
-      //   if (!isMatch) {
-      //       throw new Error("Password does not match.");
-      //   }
-      // }
+        // Recruiter
+        // if (user.role_id == "1") {
+        //   const isMatch = await User.findOne({ where: { password: user.password } });
+        //   if (!isMatch) {
+        //       throw new Error("Password does not match.");
+        //   }
+        // }
 
+        // Applicant
+        // if (user.role_id == "2") {
+        //   const isMatch = await bcrypt.compare(password, user.password);
+        //   if (!isMatch) {
+        //       throw new Error("Password does not match.");
+        //   }
+        // }
 
-      // Applicant
-      // if (user.role_id == "2") {
-      //   const isMatch = await bcrypt.compare(password, user.password);
-      //   if (!isMatch) {
-      //       throw new Error("Password does not match.");
-      //   }
-      // }
+        const isPasswordValid = await bcrypt.compare(password, user.password)
+        console.log("Login Hashed Password " + user.password)
+        if (isPasswordValid) {
+          return user
+        }
 
+        if (user.password !== password) {
+          return null
+        }
 
-     
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      console.log("Login Hashed Password " + user.password);
-      if (isPasswordValid) {
         return user
-      } 
-      if (user.password !== password) {
-        return null;
-      }
-      return user
+      })
     } catch (error) {
-      throw new Error("Login failed");
+      throw new Error("Login failed")
     }
   }
 
-  public static async register ({ name, surname, pnr, email, username, password, role_id: role_id }: registerCredentials): Promise<User | string> {
+  public static async register({
+    name,
+    surname,
+    pnr,
+    email,
+    username,
+    password,
+    role_id: role_id,
+  }: registerCredentials): Promise<User | string> {
     try {
-      if (name === '' || surname === '' || pnr === '' || email === '' || username === '' || password === '') {
-        return 'All fields are required';
-      }
-     
-      const hash = await bcrypt.hash(password, 10);
-      const userExists = await User.findOne({ where: { username } });
-      if (userExists !== null) {
-        return "User already exists";
-      }
-      const user = await User.create({ name, surname, pnr, email, username, password: hash, role_id: role_id})
-      return user
+      return await db.transaction(async () => {
+        if (!name || !surname || !pnr || !email || !username || !password) {
+          throw new Error("All fields are required")
+        }
+
+        const hash = await bcrypt.hash(password, 10)
+        const userExists = await User.findOne({ where: { username } })
+        if (userExists !== null) {
+          return "User already exists"
+        }
+        const user = await User.create({
+          name,
+          surname,
+          pnr,
+          email,
+          username,
+          password: hash,
+          role_id: role_id,
+        })
+        return user
+      })
     } catch (error) {
-      throw new Error("Register failed");
+      throw new Error("Register failed")
     }
   }
 }
 
-export default AuthService;
+export default AuthService
