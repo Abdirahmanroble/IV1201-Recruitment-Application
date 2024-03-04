79da731d5d1c19931c963e98ef70adcb5cb33e42
Nina Shamaya <shamaya@kth.se>
2024-03-04 15:16:47 +0100

    Function-based

--- front_end/src/views/LoginView/LoginView.tsx
+++ front_end/src/views/LoginView/LoginView.tsx
@@ -3,7 +3,13 @@ import { useTranslation } from "react-i18next";
 import LoginBox from "../../components/LoginBox/LoginBox";
 import "./LoginView.css";
 
-const LoginView: React.FC<ViewProps> = (props) => {
+/**
+ * Represents the view component for logging in.
+ *
+ * @param props The props passed to the component.
+ * @returns The rendered login view component.
+ */
+const LoginView = (props: ViewProps): React.ReactNode => {
   const { t } = useTranslation();
   return (
     <div className="login-view">
